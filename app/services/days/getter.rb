3a99643166eb980afdcd117d6bff92aead0fb31e
Fer Bueno <ferbuenob@gmail.com>
2021-03-07 19:45:00 -0600

    Adds number validation for day getter.

--- app/services/days/getter.rb
+++ app/services/days/getter.rb
@@ -3,11 +3,16 @@ module Days
     attr_reader :day
 
     def initialize(day)
+      Integer(day)
       @day = day
     end
 
     def call
-      Day.find_by(day: @day)
+      new_day = Day.find_by(day: @day)
+      return new_day if new_day
+
+      # Failsafe if the day does not exist
+      Days::WeatherCalculator.call(new_day)
     end
   end
 end
